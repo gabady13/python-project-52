888b58ac26170f75a73f3b28d300aed51be0aa8d
gabady13 <gabadysergei@yandex.ru>
2026-02-25 09:13:19 +0000

    1213_2502

--- src/task_manager/views/users.py
+++ src/task_manager/views/users.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
@@ -81,7 +81,17 @@ class UserDeleteView(LoginRequiredMixin, OnlySelfMixin, SafeDeleteWithProtectedE
     success_message = "Пользователь успешно удалён"
 
 
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].label = "Имя пользователя"
+        self.fields["password"].label = "Пароль"
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+
 class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
     template_name = "users/login.html"
     redirect_authenticated_user = True
 
