9aaa101ef3603f705eb7a6cb294a54440e5ab53a
gabady13 <gabadysergei@yandex.ru>
2026-02-25 07:44:16 +0000

    Revert to d53e146 (restore stable state)

---
+++ src/task_manager/views/statuses.py
@@ -0,0 +1,50 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import ModelForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from ..models import Status
+from .mixins import SafeDeleteWithProtectedErrorMixin
+
+
+class StatusForm(ModelForm):
+    class Meta:
+        model = Status
+        fields = ["name"]
+
+
+class StatusListView(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = "statuses/list.html"
+    context_object_name = "statuses"
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    form_class = StatusForm
+    template_name = "statuses/form.html"
+    success_url = reverse_lazy("statuses_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Статус успешно создан")
+        return super().form_valid(form)
+
+
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = "statuses/form.html"
+    success_url = reverse_lazy("statuses_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Статус успешно изменён")
+        return super().form_valid(form)
+
+
+class StatusDeleteView(LoginRequiredMixin, SafeDeleteWithProtectedErrorMixin, DeleteView):
+    model = Status
+    template_name = "statuses/delete.html"
+    success_url = reverse_lazy("statuses_list")
+    protected_error_message = "Невозможно удалить статус, потому что он используется"
+    success_message = "Статус успешно удалён"
---
+++ src/task_manager/views/tasks.py
@@ -0,0 +1,78 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.forms import ModelForm
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django_filters.views import FilterView
+
+from ..filters import TaskFilter
+from ..models import Task
+from .mixins import SafeDeleteWithProtectedErrorMixin
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["name", "description", "status", "executor", "labels"]
+
+
+class TaskListView(LoginRequiredMixin, FilterView):
+    model = Task
+    template_name = "tasks/list.html"
+    context_object_name = "tasks"
+    filterset_class = TaskFilter
+    queryset = (
+        Task.objects.select_related("status", "author", "executor")
+        .prefetch_related("labels")
+        .order_by("id")
+    )
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/form.html"
+    success_url = reverse_lazy("tasks_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Задача успешно создана")
+        return super().form_valid(form)
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = "tasks/show.html"
+    context_object_name = "task"
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/form.html"
+    success_url = reverse_lazy("tasks_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Задача успешно изменена")
+        return super().form_valid(form)
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author_id == self.request.user.id
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+        messages.error(self.request, "Задачу может удалить только её автор")
+        return redirect("tasks_list")
+
+
+class TaskDeleteView(LoginRequiredMixin, OnlyAuthorMixin, SafeDeleteWithProtectedErrorMixin, DeleteView):
+    model = Task
+    template_name = "tasks/delete.html"
+    success_url = reverse_lazy("tasks_list")
+    protected_error_message = "Невозможно удалить задачу, потому что она используется"
+    success_message = "Задача успешно удалена"
