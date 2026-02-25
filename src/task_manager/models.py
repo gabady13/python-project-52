c7f079130fe80305892350360f4aa637be2eada2
gabady13 <gabadysergei@yandex.ru>
2026-02-25 11:30:35 +0000

    fix linter

--- src/task_manager/models.py
+++ src/task_manager/models.py
@@ -3,15 +3,26 @@ from django.conf import settings
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Имя')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name='Имя'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
 
     def __str__(self) -> str:
         return self.name
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Имя')
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name='Имя'
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата создания'
