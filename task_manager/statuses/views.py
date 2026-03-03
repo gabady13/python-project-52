67a057310b2144f85b780ee94161ce2212a8c8bd
gabady13 <gabadysergei@yandex.ru>
2026-03-03 07:05:22 +0000

    Move TaskFilter into tasks app

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import ModelForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from ..models import Status
+from task_manager.views.mixins import SafeDeleteWithProtectedErrorMixin
+
+
+class StatusForm(ModelForm):
+    class Meta:
+        model = Status
+        fields = ["name"]
+
+
+class StatusListView(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = "statuses/list.html"
+    context_object_name = "statuses"
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    form_class = StatusForm
+    template_name = "statuses/form.html"
+    success_url = reverse_lazy("statuses_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Статус успешно создан")
+        return super().form_valid(form)
+
+
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = "statuses/form.html"
+    success_url = reverse_lazy("statuses_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Статус успешно изменен")
+        return super().form_valid(form)
+
+
+class StatusDeleteView(
+    LoginRequiredMixin,
+    SafeDeleteWithProtectedErrorMixin,
+    DeleteView
+    ):
+    model = Status
+    template_name = "statuses/delete.html"
+    success_url = reverse_lazy("statuses_list")
+    protected_error_message = (
+        "Невозможно удалить статус, потому что он используется"
+    )
+    success_message = "Статус успешно удален"
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,171 @@
-from django.shortcuts import render
+from django import forms
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-# Create your views here.
+from task_manager.views.mixins import SafeDeleteWithProtectedErrorMixin
+
+
+class UsersListView(ListView):
+    model = User
+    template_name = "users/user_list.html"
+    context_object_name = "users"
+
+    def get_queryset(self):
+        return User.objects.order_by("id")
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
+        labels = {
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+            "username": "Имя пользователя",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+        self.fields["password1"].label = "Пароль"
+        self.fields["password2"].label = "Подтверждение пароля"
+
+
+class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        required=False,
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
+        labels = {
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+            "username": "Имя пользователя",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+    def clean(self):
+        cleaned = super().clean()
+        p1 = cleaned.get("password1")
+        p2 = cleaned.get("password2")
+
+        if p1 or p2:
+            if p1 != p2:
+                self.add_error("password2", "Пароли не совпадают")
+            else:
+                validate_password(p1, self.instance)
+
+        return cleaned
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        p1 = self.cleaned_data.get("password1")
+        if p1:
+            user.set_password(p1)
+        if commit:
+            user.save()
+        return user
+
+
+class OnlySelfMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.get_object().id == self.request.user.id
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/user_form.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Пользователь успешно зарегистрирован")
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, OnlySelfMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = "users/user_form.html"
+    success_url = reverse_lazy("users_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Пользователь успешно изменен")
+        return super().form_valid(form)
+
+
+class UserDeleteView(
+    LoginRequiredMixin,
+    OnlySelfMixin,
+    SafeDeleteWithProtectedErrorMixin,
+    DeleteView,
+):
+    model = User
+    template_name = "users/user_confirm_delete.html"
+    success_url = reverse_lazy("users_list")
+    protected_error_message = (
+        "Невозможно удалить пользователя, потому что он используется"
+    )
+    success_message = "Пользователь успешно удален"
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].label = "Имя пользователя"
+        self.fields["password"].label = "Пароль"
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = "users/login.html"
+    redirect_authenticated_user = True
+
+    def form_valid(self, form):
+        messages.success(self.request, "Вы залогинены")
+        return super().form_valid(form)
+
+
+class UserLogoutView(LogoutView):
+    http_method_names = ["post"]
+
+    def post(self, request, *args, **kwargs):
+        messages.info(self.request, "Вы разлогинены")
+        return super().post(request, *args, **kwargs)
